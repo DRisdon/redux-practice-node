9afbd13964eed1b41b0a8008a0bd8279fb907c7a
Daniel Risdon <drisdon07@gmail.com>
2018-04-23 11:29:13 -0400

    basic index.js setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const postController = require('./controllers/posts')
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/posts', postController)
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+  res.redirect('/posts');
+});
+
+app.post('/', (req, res, next) => {
+  res.redirect('/posts');
+});
